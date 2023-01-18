1b4f64a178b19a00154a6c1ca7da1fe8f8c78463
Ankit Kumar <ankitkumar790177@gmail.com>
2023-01-18 19:28:54 +0530

    Typing Animation added

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* Typing Animation */
+var typed = new Typed(".typing", {
+    strings: ["Front-End Developer", "Web Developer"],
+    typeSpeed: 50,
+    loop: true,
+    backDelay: 900,
+    backSpeed: 30,
+})
